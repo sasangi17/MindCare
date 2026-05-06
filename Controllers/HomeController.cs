1fe686893d961f864cf5b1f52cf2de84870ae58c
29849 <135608399+sasangi17@users.noreply.github.com>
2026-05-06 17:28:51 +0530

    Update HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using MindCare_Pro.Areas.Identity.Data;
 using MindCare_Pro.Models;
 using System.Diagnostics;
 
@@ -7,20 +9,39 @@ namespace MindCare_Pro.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(
+            ILogger<HomeController> logger,
+            UserManager<ApplicationUser> userManager)
         {
             _logger = logger;
+            _userManager = userManager;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            // If user already logged in ? go to dashboard
+            // IF USER ALREADY LOGGED IN
             if (User.Identity.IsAuthenticated)
             {
-                return RedirectToAction("Index", "Dashboard");
+                var user = await _userManager.GetUserAsync(User);
+
+                // ADMIN USER
+                if (await _userManager.IsInRoleAsync(user, "Admin"))
+                {
+                    return RedirectToAction(
+                        "Index",
+                        "Dashboard",
+                        new { area = "Admin" });
+                }
+
+                // NORMAL USER
+                return RedirectToAction(
+                    "Index",
+                    "Dashboard");
             }
 
+            // NOT LOGGED IN
             return View();
         }
 
@@ -29,10 +50,19 @@ namespace MindCare_Pro.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [ResponseCache(
+            Duration = 0,
+            Location = ResponseCacheLocation.None,
+            NoStore = true)]
+
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(
+                new ErrorViewModel
+                {
+                    RequestId = Activity.Current?.Id
+                    ?? HttpContext.TraceIdentifier
+                });
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -35,6 +35,7 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "areas",
